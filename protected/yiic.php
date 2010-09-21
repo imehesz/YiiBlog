8d7beca056b124b913afd0e0c08636df6e95d0b7
Imre Mehesz <imremehesz@WKST-TDS16-LX.(none)>
2010-09-21 13:01:57 -0400

    added classic Yii structure

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../../yii-1.1.4.r2429/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
