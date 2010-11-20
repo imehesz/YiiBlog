6d9bf91399644bf4839a7ce5c9250fff85d242bf
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2010-11-19 20:39:09 -0500

    added UserMenu Cportlet to handle the menu

---
+++ protected/components/UserMenu.php
@@ -0,0 +1,20 @@
+<?php
+	Yii::import( 'zii.widgets.CPortlet'  );
+
+	/**
+	 * 
+	 **/
+	class UserMenu extends CPortlet
+	{
+		public function init()
+		{
+			$this->title=CHtml::encode(Yii::app()->user->name);
+			parent::init();
+		}
+
+		protected function renderContent()
+		{
+			// code...
+			$this->render( 'userMenu' );
+		}
+	}
--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -7,6 +7,7 @@
 	</div>
 	<div class="span-5 last">
 		<div id="sidebar">
+			<?php if(!Yii::app()->user->isGuest) $this->widget('UserMenu'); ?>
 		<?php
 			$this->beginWidget('zii.widgets.CPortlet', array(
 				'title'=>'Operations',
