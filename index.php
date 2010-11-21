bfbf142a38fe3999bf77803c924f8a3f45f3a981
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2010-11-20 23:32:43 -0500

    started reconfiguring the environment constants for good measure

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../../yii-1.1.4.r2429/framework/yii.php';
 
+defined( 'ENV' ) or define( 'ENV', 'development' );
+
 $mehesz_config=dirname( __FILE__ ) . '/protected/mehesz/config.php';
 
 if( file_exists( $mehesz_config ) )
@@ -14,20 +16,20 @@ else
 	die( 'Missing configuration file, expected here: ' . $mehesz_config );
 }
 
-if( ! isset( $_SERVER['SERVER_NAME'] ) || stristr($_SERVER['SERVER_NAME'], 'local' ) )
+if( ENV != 'prod' && ENV != 'production')
 {
 	error_reporting (E_ALL);
 	ini_set("display_errors", 1);
 
     $config=dirname(__FILE__).'/protected/config/development.php';
+	defined('YII_DEBUG') or define('YII_DEBUG',true);
 }
 else
 {
     $config=dirname(__FILE__).'/protected/config/production.php';
+	defined('YII_DEBUG') or define('YII_DEBUG',false);
 }
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
